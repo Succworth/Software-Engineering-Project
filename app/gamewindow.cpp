756a67cc55365acee2143d3100aef41c85dd0e74
steviegalluscio <galluscio.stevie@gmail.com>
2022-04-17 13:24:53 -0400

    User choice selection fixes

--- app/gamewindow.cpp
+++ app/gamewindow.cpp
@@ -58,9 +58,6 @@ void GameWindow::displayQuestion() {
     ui->Choice_C->setText(choices[2]);
     ui->Choice_D->setText(choices[3]);
 
-    QDir exeDir(QCoreApplication::applicationDirPath());
-    qDebug() <<  exeDir.path()+"/Assets.qrc";
-    QResource::registerResource(exeDir.path()+"/Assets.qrc");
     QString path = ":/Assets/Common Words and Phrases " + QString::number(lessonNumber) + "/";
     if(lessonNumber == 0) { path = ":/Assets/Alphabet/"; }
     QMovie *movie = new QMovie(path + currQuestion + ".gif");
@@ -73,15 +70,19 @@ void GameWindow::displayQuestion() {
 //Reset button colors
 void GameWindow::resetButtons() {
     ui->Choice_A->setStyleSheet("* { border-color: #051a39 }");
+    ui->Choice_A->setEnabled(true);
     ui->Choice_A->update();
 
     ui->Choice_B->setStyleSheet("* { border-color: #051a39 }");
+    ui->Choice_B->setEnabled(true);
     ui->Choice_B->update();
 
     ui->Choice_C->setStyleSheet("* { border-color: #051a39 }");
+    ui->Choice_C->setEnabled(true);
     ui->Choice_C->update();
 
     ui->Choice_D->setStyleSheet("* { border-color: #051a39 }");
+    ui->Choice_D->setEnabled(true);
     ui->Choice_D->update();
 
 }
@@ -96,15 +97,21 @@ void GameWindow::on_Next_clicked()
 
 void GameWindow::on_Choice_A_clicked()
 {
-    resetButtons();
     QString text = ui->Choice_A->text();
     if (text.compare(currQuestion) == 0) {
-        ui->Choice_A->setStyleSheet("* { border-color: #00ff00 }");
+        ui->Choice_A->setStyleSheet("background-color: #607cff;color: #ffffff;border-style: solid;border-width: 3px;border-radius: 3px;border-color: #00ff00;padding: 5px;");
+        ui->Choice_B->setEnabled(false);
+        ui->Choice_B->update();
+        ui->Choice_C->setEnabled(false);
+        ui->Choice_C->update();
+        ui->Choice_D->setEnabled(false);
+        ui->Choice_D->update();
         ui->Choice_A->update();
         ui->Next->show();
     }
     else {
         ui->Choice_A->setStyleSheet("* { border-color: #ff0000 }");
+        ui->Choice_A->setEnabled(false);
         ui->Choice_A->update();
     }
 }
@@ -112,15 +119,19 @@ void GameWindow::on_Choice_A_clicked()
 
 void GameWindow::on_Choice_B_clicked()
 {
-    resetButtons();
+    //resetButtons();
     QString text = ui->Choice_B->text();
     if (text.compare(currQuestion) == 0) {
-        ui->Choice_B->setStyleSheet("* { border-color: #00ff00 }");
+        ui->Choice_B->setStyleSheet("background-color: #607cff;color: #ffffff;border-style: solid;border-width: 3px;border-radius: 3px;border-color: #00ff00;padding: 5px;");
+        ui->Choice_A->setEnabled(false);
+        ui->Choice_C->setEnabled(false);
+        ui->Choice_D->setEnabled(false);;
         ui->Choice_B->update();
         ui->Next->show();
     }
     else {
         ui->Choice_B->setStyleSheet("* { border-color: #ff0000 }");
+        ui->Choice_B->setEnabled(false);
         ui->Choice_B->update();
     }
 }
@@ -128,15 +139,18 @@ void GameWindow::on_Choice_B_clicked()
 
 void GameWindow::on_Choice_C_clicked()
 {
-    resetButtons();
     QString text = ui->Choice_C->text();
     if (text.compare(currQuestion) == 0) {
-        ui->Choice_C->setStyleSheet("* { border-color: #00ff00 }");
+        ui->Choice_C->setStyleSheet("background-color: #607cff;color: #ffffff;border-style: solid;border-width: 3px;border-radius: 3px;border-color: #00ff00;padding: 5px;");
+        ui->Choice_A->setEnabled(false);
+        ui->Choice_B->setEnabled(false);
+        ui->Choice_D->setEnabled(false);
         ui->Choice_C->update();
         ui->Next->show();
     }
     else {
         ui->Choice_C->setStyleSheet("* { border-color: #ff0000 }");
+        ui->Choice_C->setEnabled(false);
         ui->Choice_C->update();
     }
 }
@@ -144,15 +158,18 @@ void GameWindow::on_Choice_C_clicked()
 
 void GameWindow::on_Choice_D_clicked()
 {
-    resetButtons();
     QString text = ui->Choice_D->text();
     if (text.compare(currQuestion) == 0) {
-        ui->Choice_D->setStyleSheet("* { border-color: #00ff00 }");
+        ui->Choice_D->setStyleSheet("background-color: #607cff;color: #ffffff;border-style: solid;border-width: 3px;border-radius: 3px;border-color: #00ff00;padding: 5px;");
+        ui->Choice_A->setEnabled(false);
+        ui->Choice_B->setEnabled(false);
+        ui->Choice_C->setEnabled(false);
         ui->Choice_D->update();
         ui->Next->show();
     }
     else {
         ui->Choice_D->setStyleSheet("* { border-color: #ff0000 }");
+        ui->Choice_D->setEnabled(false);
         ui->Choice_D->update();
     }
 }
